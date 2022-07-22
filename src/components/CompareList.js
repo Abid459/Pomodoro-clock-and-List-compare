0aeb3e3ae23d1732107114a8325a0526b07b4be2
ABID <abid45986@gmail.com>
2022-07-22 23:45:25 +0600

    All item common-item and unique-item functionality added

--- src/components/CompareList.js
+++ src/components/CompareList.js
@@ -7,79 +7,102 @@ const CompareList = () => {
 
     const [outputA, setOutputA] = useState()
     const [outputB, setOutputB] = useState()
+    const [outputAll, setOutputAll] = useState()
+    const [commonEl, setCommonEl] = useState()
+    const [uniquA, setUniquA] = useState()
+    const [uniquB, setUniquB] = useState()
+
+    function mapArr(arr) {
+        let count = 1;
+        const output = arr.map(item => {
+            return <div key={count} className="overflow-x-auto">
+                <p className='bg-slate-200 mb-1 p-3 rounded font-semibold'>{count++}. {item}</p>
+            </div>
+        })
+        return output;
+    }
 
     const handleCompare = () => {
         const inputA = inputARef.current.value;
         const inputB = inputBRef.current.value;
-        // const output = inputBRef.current.value;
-
+ 
 
         const inputA_Arr = inputA.split('\n');
         const inputB_Arr = inputB.split('\n');
-        console.log(inputA_Arr);
+        console.log(inputB_Arr);
+        let totalArr = [...inputA_Arr, ...inputB_Arr];
 
-        let countA = 1
-        const outputA = inputA_Arr.map(item => {
-            return <div class="overflow-x-auto">
-                <p className='bg-slate-200 mb-1 p-3 font-semibold'>{countA++}. {item}</p>
-            </div>
-        })
-        setOutputA(outputA)
+        const commonArr = inputA_Arr.filter(elA => inputB_Arr.indexOf(elA) !== -1)
+        const uniqueInA = inputA_Arr.filter(elA => inputB_Arr.indexOf(elA) === -1)
+        const uniqueInB = inputB_Arr.filter(elB => inputA_Arr.indexOf(elB) === -1)
+        console.log("This is unique Aarr", uniqueInA)
+        console.log("This is unique Barr", uniqueInB)
 
-        let countB = 1;
-        const outputB = inputB_Arr.map(item => {
-            return <div class="overflow-x-auto">
-                <p className='bg-slate-200 mb-1 p-3 font-semibold'>{countB++}. {item}</p>
-            </div>
-        })
-        setOutputB(outputB)
+        setOutputA(mapArr(inputA_Arr))
+        setOutputB(mapArr(inputB_Arr))
+        setOutputAll(mapArr(totalArr))
+        setCommonEl(mapArr(commonArr))
+        setUniquA(mapArr(uniqueInA))
+        setUniquB(mapArr(uniqueInB))
 
 
 
     }
 
     return (
-        <div className='h-screen bg-slate-600'>
-            <div className='flex w-screen justify-center  items-center pt-10'>
-                <div className='bg-slate-400 w-full text-center text-'>
-                    <h3>List-1</h3>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Your bio</span>
-                            <span class="label-text-alt">Alt label</span>
+        <div className='min-h-screen bg-slate-600 px-10'>
+            <div className='flex  justify-center  items-center pt-10'>
+                <div className='bg-slate-400 w-full text-center text- p-5'>
+                    <h3 className='text-xl mb-4'>List-A</h3>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Enter your First list and separate them by new line</span>
                         </label>
-                        <textarea ref={inputARef} class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
-                        <label class="label">
-                            <span class="label-text-alt">Your bio</span>
-                            <span class="label-text-alt">Alt label</span>
-                        </label>
+                        <textarea ref={inputARef} className="textarea textarea-bordered h-40" placeholder="List-A"></textarea>
                     </div>
                 </div>
-                <div className='bg-slate-400 w-full text-center text-'>
-                    <h3>List-1</h3>
-                    <div class="form-control">
-                        <label class="label">
-                            <span class="label-text">Your bio</span>
-                            <span class="label-text-alt">Alt label</span>
+                <div className='bg-slate-400 w-full text-center text- p-5'>
+                    <h3 className='text-xl mb-4'>List-B</h3>
+                    <div className="form-control">
+                    <label className="label">
+                            <span className="label-text">Enter your Second list and separate them by new line</span>
                         </label>
-                        <textarea ref={inputBRef} class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
-                        <label class="label">
-                            <span class="label-text-alt">Your bio</span>
-                            <span class="label-text-alt">Alt label</span>
-                        </label>
+                        <textarea ref={inputBRef} className="textarea textarea-bordered h-40" placeholder="List-B"></textarea>
+
                     </div>
                 </div>
 
             </div>
-            <div className='flex justify-center mt-10'>
-                <button onClick={handleCompare} class="btn btn-outline">compare</button>
+            <div className='flex justify-center my-10'>
+                <button onClick={handleCompare} className="btn ">COMPUTE</button>
             </div>
-            <div className='grid grid-cols-2 gap-5'>
+            <div className='flex flex-wrap justify-between bg-slate-50 p-10'>
                 <div className=''>
+                    <h3 className='font-semibold mb-5 bg-slate-300 p-4 text-center'>Items inside List-A</h3>
                     {outputA}
                 </div>
                 <div className=''>
+                    <h3 className='font-semibold mb-5 bg-slate-300 p-4 text-center'>Items inside List-B</h3>
                     {outputB}
+                </div>
+                <div className=''>
+                    <h3 className='font-semibold mb-5 bg-slate-300 p-4 text-center'>Items inside List-A and List-B</h3>
+                    {outputAll}
+                </div>
+                <div className=''>
+                    <h3 className='font-semibold mb-5 bg-slate-300 p-4 text-center'>Common between them</h3>
+                    {commonEl}
+                </div>
+                <div className=''>
+                    <h3 className='font-semibold mb-5 bg-slate-300 p-4 text-center'>Unque between them</h3>
+                    <div>
+                        <h4 className='text-center bg-slate-300 p-2'>Unique in A</h4>
+                        {uniquA}
+                    </div>
+                    <div>
+                        <h4 className='text-center bg-slate-300 p-2'>Unique in B</h4>
+                        {uniquB}
+                    </div>
                 </div>
             </div>
         </div>
